2f21fa74c05cebaa5e07b80189503bf6e6f98778
ABuffaloHerd <62997882+ABuffaloHerd@users.noreply.github.com>
2023-09-17 16:29:43 +1200

    i forgor

---
+++ config.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+def init_config(servers):
+    if not os.path.exists('config.json'):
+        with open('config.json', 'w') as f:
+            file = {}
+            for server in servers:
+                file[server] = {"alerts" : False}
+
+            json.dump(file, f)
+
+
+def get_config(server):
+    with open('config.json', 'r') as f:
+        return json.load(f)[server]
+    
+def is_alerts(server):
+    return get_config(server)['alerts']
+
+def set_alerts(server, value : bool):
+    with open('config.json', 'r') as f:
+        data = json.load(f)
+        data[server]['alerts'] = value
+    with open('config.json', 'w') as f:
+        json.dump(data, f)
